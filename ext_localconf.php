571827e0ccb31b5fdda1ef979082f1e18d06552c
Frodo Podschwadek <frodo.podschwadek@gmail.com>
2024-08-07 11:25:46 +0100

    Remove TYPO3_MODE references and clean up namespaces a bit in ext_localconf file.
    chore: make things work for TYPO3 12.4

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,96 +1,110 @@
 <?php
-if (!defined('TYPO3')) {
-    die('Access denied.');
-}
+defined('TYPO3') or die();
+
+use Digicademy\Lod\Backend\Form\Element\EnhancedGroupElement;
+use Digicademy\Lod\Backend\Form\FieldControl\EnhancedAddRecord;
+use Digicademy\Lod\Backend\Form\FieldWizard\EnhancedTableList;
+use Digicademy\Lod\Controller\{
+    ApiController,
+    SerializerController,
+    VocabularyController
+};
+use Digicademy\Lod\Resolver\{
+    HttpResolver,
+    HttpsResolver,
+    T3Resolver
+};
+use TYPO3\CMS\Backend\Form\Element\GroupElement;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+use TYPO3\CMS\Core\Imaging\IconRegistry;
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
 
 // PLUGINS
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+ExtensionUtility::configurePlugin(
     'Lod',
     'Vocabulary',
     array(
-        \Digicademy\Lod\Controller\VocabularyController::class => 'show',
+        VocabularyController::class => 'show',
     ),
     array(
-        \Digicademy\Lod\Controller\VocabularyController::class => '',
+        VocabularyController::class => '',
     )
 );
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+ExtensionUtility::configurePlugin(
     'Lod',
     'Api',
     array(
-        \Digicademy\Lod\Controller\ApiController::class => 'about',
+        ApiController::class => 'about',
     ),
     array(
-        \Digicademy\Lod\Controller\ApiController::class => 'about',
+        ApiController::class => 'about',
     )
 );
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+ExtensionUtility::configurePlugin(
     'Lod',
     'Serializer',
     array(
-        \Digicademy\Lod\Controller\SerializerController::class => 'iri',
+        SerializerController::class => 'iri',
     ),
     array(
-        \Digicademy\Lod\Controller\SerializerController::class => '',
+        SerializerController::class => '',
     )
 );
 
 // REGISTERES URI RESOLVER
 
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['lod']['resolver'] = [
-    't3' => Digicademy\Lod\Resolver\T3Resolver::class,
-    'http' => Digicademy\Lod\Resolver\HttpResolver::class,
-    'https' => Digicademy\Lod\Resolver\HttpsResolver::class,
+    't3' => T3Resolver::class,
+    'http' => HttpResolver::class,
+    'https' => HttpsResolver::class,
 ];
 
 // TYPE ICONS
 
-if (TYPO3_MODE === 'BE') {
-
-    // register icons for IRIs
-    $icons = [
-        'ext-lod-type-default' => 'tx_lod_domain_model_iri.svg',
-        'ext-lod-type-class' => 'tx_lod_domain_model_iri.svg',
-        'ext-lod-type-property' => 'tx_lod_domain_model_property.svg',
-    ];
-    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-    foreach ($icons as $identifier => $path) {
-        if (!$iconRegistry->isRegistered($identifier)) {
-            $iconRegistry->registerIcon(
-                $identifier,
-                \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-                ['source' => 'EXT:lod/Resources/Public/Icons/' . $path]
-            );
-        }
+// register icons for IRIs
+$icons = [
+    'ext-lod-type-default' => 'tx_lod_domain_model_iri.svg',
+    'ext-lod-type-class' => 'tx_lod_domain_model_iri.svg',
+    'ext-lod-type-property' => 'tx_lod_domain_model_property.svg',
+];
+$iconRegistry = GeneralUtility::makeInstance(IconRegistry::class);
+foreach ($icons as $identifier => $path) {
+    if (!$iconRegistry->isRegistered($identifier)) {
+        $iconRegistry->registerIcon(
+            $identifier,
+            SvgIconProvider::class,
+            ['source' => 'EXT:lod/Resources/Public/Icons/' . $path]
+        );
     }
-
-    // register tcemain hooks for table tracking, statement synchronization and identifier generation
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = 'Digicademy\Lod\Hooks\Backend\DataHandler';
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass'][] = 'Digicademy\Lod\Hooks\Backend\DataHandler';
-
-    // add modified addRecord fieldControl (make it reusable for different types of records)
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][138471234123] = [
-       'nodeName' => 'enhancedAddRecord',
-       'priority' => 30,
-       'class' => Digicademy\Lod\Backend\Form\FieldControl\EnhancedAddRecord::class
-    ];
-
-    // add modified tableList fieldWizard (take out hard coded connection to elementBrowser)
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1617617718] = [
-       'nodeName' => 'enhancedTableList',
-       'priority' => 30,
-       'class' => Digicademy\Lod\Backend\Form\FieldWizard\EnhancedTableList::class
-    ];
-
-    // XCLASS group field to change hardcoded HTML arrangement of fieldControl
-    // we don't register a new formEngine node and use XCLASS since has problems in data handling (tested)
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][TYPO3\CMS\Backend\Form\Element\GroupElement::class] = [
-       'className' => Digicademy\Lod\Backend\Form\Element\EnhancedGroupElement::class
-    ];
 }
+
+// register tcemain hooks for table tracking, statement synchronization and identifier generation
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = 'Digicademy\Lod\Hooks\Backend\DataHandler';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass'][] = 'Digicademy\Lod\Hooks\Backend\DataHandler';
+
+// add modified addRecord fieldControl (make it reusable for different types of records)
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][138471234123] = [
+   'nodeName' => 'enhancedAddRecord',
+   'priority' => 30,
+   'class' => EnhancedAddRecord::class
+];
+
+// add modified tableList fieldWizard (take out hard coded connection to elementBrowser)
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1617617718] = [
+   'nodeName' => 'enhancedTableList',
+   'priority' => 30,
+   'class' => EnhancedTableList::class
+];
+
+// XCLASS group field to change hardcoded HTML arrangement of fieldControl
+// we don't register a new formEngine node and use XCLASS since has problems in data handling (tested)
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][GroupElement::class] = [
+   'className' => EnhancedGroupElement::class
+];
 
 // exclude extension parameters from cHash generation
 $GLOBALS['TYPO3_CONF_VARS']['FE']['cacheHash']['excludedParameters'][] = 'tx_lod_api[iri]';
